370ef35dadc8bc9fb83ffe0fbd361fb9ba6b41e0
FalconTube <yannic.falke@gmail.com>
2025-03-14 23:20:04 +0100

    revert "Refactoring"

--- lib/widgets/all_countries_widget.dart
+++ lib/widgets/all_countries_widget.dart
@@ -1,12 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:holiday_map/logging/logger.dart';
 import 'package:holiday_map/main.dart';
 import 'package:holiday_map/providers/all_countries_provider.dart';
 import 'package:holiday_map/widgets/color_legend_widget.dart';
-import 'package:holiday_map/widgets/custom_date_picker_widget.dart';
 import 'package:syncfusion_flutter_maps/maps.dart';
-
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:color_map/color_map.dart';
 
 class AllCountriesWidget extends ConsumerWidget {
@@ -81,10 +81,47 @@ class AllCountriesWidget extends ConsumerWidget {
                           tooltipSettings: MapTooltipSettings(
                               color: const Color.fromRGBO(30, 28, 37, 1)),
                           sublayers: <MapSublayer>[
-                            NutsSublayer(
-                                nutsSource: nutsSource,
-                                mapController: _mapController,
-                                data: data)
+                            MapShapeSublayer(
+                              key: UniqueKey(),
+                              source: nutsSource,
+                              controller: _mapController,
+                              strokeWidth: 0.5,
+                              // showDataLabels: true,
+                              color: Colors.grey.withValues(alpha: 0.0),
+                              strokeColor: Colors.black.withValues(alpha: 0.1),
+                              shapeTooltipBuilder:
+                                  (BuildContext context, int index) {
+                                final totalDays = data.data[index].totalDays;
+                                final division = data.data[index].division;
+                                final holidays = data.data[index].holidays;
+                                // Build holiday display text
+                                String holFormatted = "";
+                                for (final h in holidays) {
+                                  final name = h.nameEN ?? h.name;
+                                  final start =
+                                      h.start.toString().split(' ')[0];
+                                  final end = h.end.toString().split(' ')[0];
+                                  final thisHol =
+                                      "\n\n$name\n$start \u2014 $end";
+                                  holFormatted += thisHol;
+                                }
+                                final text = Text(
+                                  'Division: $division\nOverlap Days: $totalDays$holFormatted',
+                                  textAlign: TextAlign.center,
+                                  softWrap: true,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                );
+                                return ConstrainedBox(
+                                    constraints: BoxConstraints(maxWidth: 300),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: text,
+                                    ));
+                              },
+                            )
                           ],
                           zoomPanBehavior: zoomPanBehavior,
                         ),
@@ -102,64 +139,41 @@ class AllCountriesWidget extends ConsumerWidget {
                   ]),
             ),
           ),
-          MyDatePicker(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 3,
+            child: SfDateRangePicker(
+                headerHeight: 50,
+                showNavigationArrow: true,
+                monthViewSettings: DateRangePickerMonthViewSettings(
+                    enableSwipeSelection: kIsWeb ? true : false),
+                toggleDaySelection: true,
+                selectionMode: DateRangePickerSelectionMode.range,
+                initialSelectedDate: DateTime.now(),
+                minDate: DateTime(2025),
+                maxDate: DateTime(2028),
+                extendableRangeSelectionDirection:
+                    ExtendableRangeSelectionDirection.forward,
+                onSelectionChanged:
+                    (DateRangePickerSelectionChangedArgs args) async {
+                  final PickerDateRange selectedRange = args.value;
+                  final startDate = selectedRange.startDate;
+                  final endDate = selectedRange.endDate;
+                  if (startDate == null || endDate == null) return;
+                  final days = DateTimeRange(start: startDate, end: endDate)
+                      .duration
+                      .inDays;
+                  final out = findHolidaysForDate(startDate, endDate);
+                  // Reset
+                  await ref.read(nutsDataProvider.notifier).resetData();
+
+                  // Update
+                  await ref
+                      .read(nutsDataProvider.notifier)
+                      .updateMultipleIDs(out, days);
+                }),
+          ),
         ],
       ),
-    );
-  }
-}
-
-class NutsSublayer extends MapSublayer {
-  const NutsSublayer({
-    super.key,
-    required this.nutsSource,
-    required MapShapeLayerController mapController,
-    required this.data,
-  }) : _mapController = mapController;
-
-  final MapShapeSource nutsSource;
-  final MapShapeLayerController _mapController;
-  final MapCountryDataAndDays data;
-
-  @override
-  Widget build(BuildContext context) {
-    return MapShapeSublayer(
-      key: UniqueKey(),
-      source: nutsSource,
-      controller: _mapController,
-      strokeWidth: 0.5,
-      // showDataLabels: true,
-      color: Colors.grey.withValues(alpha: 0.0),
-      strokeColor: Colors.black.withValues(alpha: 0.1),
-      shapeTooltipBuilder: (BuildContext context, int index) {
-        final totalDays = data.data[index].totalDays;
-        final division = data.data[index].division;
-        final holidays = data.data[index].holidays;
-        // Build holiday display text
-        String holFormatted = "";
-        for (final h in holidays) {
-          final name = h.nameEN ?? h.name;
-          final start = h.start.toString().split(' ')[0];
-          final end = h.end.toString().split(' ')[0];
-          final thisHol = "\n\n$name\n$start \u2014 $end";
-          holFormatted += thisHol;
-        }
-        final text = Text(
-          'Division: $division\nOverlap Days: $totalDays$holFormatted',
-          textAlign: TextAlign.center,
-          softWrap: true,
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-          ),
-        );
-        return ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 300),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: text,
-            ));
-      },
     );
   }
 }
